509abcbef1d09a0ad5e1f276c775a7df6483e147
Hugo Rossi <hugo.hrossi@gmail.com>
2024-01-11 19:32:50 -0300

    My first action is ready

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
+  const regex = core.getInput('regex');
+  const data = core.getInput('data-to-validate');
+
+  console.log('Validating the data:');
+  console.log(data);
+
+  console.log('Using the REGEX:');
+  console.log(regex);
+
+  if (data.test(regex)) {
+    core.setOutput("time", time);  
+  } else {
+    core.setFailed("OOOPS! FAILED");
+  }
+
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
